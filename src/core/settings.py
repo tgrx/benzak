0031c9e70ea48dfa7dd993b3d649339f1f3fe719
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-10-02 12:54:56 +0300

    configured allowed hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wpo_brp%osd#_!^yq!vg4og+uel=!10k+9z5=e$9_8bhwm^wo%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "benzak.herokuapp.com"]
 
 
 # Application definition
