8b7561d4fb23503c936c1f16045bd881d7d49978
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-10-07 17:43:56 +0300

    Splitted templates

--- src/home/views.py
+++ src/home/views.py
@@ -1,11 +1,22 @@
-from django.core.signing import Signer
+from typing import NamedTuple
+
 from django.shortcuts import render
 
 
-def get_alco():
-    signer = Signer()
-    return tuple((v, signer.sign(v)) for v in ["Jim Beam", "Vodka", "Pivo"])
+class FuelData(NamedTuple):
+    color: str
+    price: float
+
+
+def get_prices():
+    return {
+        "98": FuelData("#9500D0", 1.9),
+        "95": FuelData("#E80000", 1.7),
+        "92": FuelData("#C19B10", 1.5),
+        "DT": FuelData("#00BE06", 1.8),
+        "LPG": FuelData("#0088D0", 0.9),
+    }
 
 
 def index(request):
-    return render(request, "index.html", {"alco": get_alco(), "title": "Main"})
+    return render(request, "index.html", {"prices": get_prices().items()})
