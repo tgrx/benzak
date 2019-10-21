d0dfc5fac1ae2d006b0de1232d58454bc2f1b6ed
Alexander Sidorov <alex.n.sidorov@gmail.com>
2019-10-21 20:01:49 +0300

    xxx endpoint for testing post requests

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.http import HttpRequest
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def xxx(request: HttpRequest):
+    from django.http import HttpResponse
+    import json
+    return HttpResponse(json.dumps({"method": request.method}), content_type="application/json")
+
 
 urlpatterns = [
     path("", include("home.urls")),
@@ -7,4 +17,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("dynamics/", include("dynamics.urls")),
     path("graphics/", include("graphics.urls")),
+    path("xxx/", xxx),
 ]
